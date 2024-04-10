26da09d4030ffb1a12811fbcca24a0b03f447cb4
Simone Dario <simone.dario10@gmail.com>
2024-04-10 11:53:39 +0200

    Lab algo maybe finished

--- max_e_min/README.txt
+++ max_e_min/README.txt
@@ -1,1 +1,3 @@
-Scrivi un programma in C++ che legga da input una sequenza di numeri interi e determini l'indice dell'elemento che è maggiore o uguale a tutti gli elementi precedenti e minore o uguale a tutti gli elementi successivi. Se tale elemento non esiste, stampa -1.
+Scrivi un programma in C++ che legga da input una sequenza di numeri interi e determini l'indice dell'elemento che
+è maggiore o uguale a tutti gli elementi precedenti e minore o uguale a tutti gli elementi successivi. 
+Se tale elemento non esiste, stampa -1.
--- max_e_min/main.cpp
+++ max_e_min/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include <sstream>
+#include <fstream>
+#include <string>
 #include <vector>
+#include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -26,15 +29,53 @@ istream &operator >> (istream &in, vector<int> &vec) {
     return in;
 }
 
+int findNum(vector<int> numbers){
+    int ris=-1;
+    bool check = false;
+    for (int i = 0; i < numbers.size(); i++){
+        for (int j = 0; j < numbers.size(); j++){
+            if(j < i){ // If j is less than i
+                if((numbers[i] >= numbers[j]) && (check == false)){
+                    ris = i;
+                }else{
+                    check = true;
+                }
+            }else{ // If j is greater than or equal to i
+                if((numbers[j] >= numbers[i]) && (check == false)){
+                    ris = i;
+                }
+                else{
+                    check = true;
+                }
+            }
+        }
+        if (check == false){
+            return ris;
+        }else{
+            check = false;
+            ris = -1;
+        }
+    }
+    return -1;
+}
+
 int main() {
     vector<int> numbers;
 
-    // Leggi i numeri da cin
-    cout << "Inserisci una sequenza di numeri: ";
-    cin >> numbers;
+    ifstream file("input.txt");
+    ofstream outputFile("output.txt");
+    string line;
 
-    // Stampa i numeri
-    cout << "I numeri inseriti sono: " << numbers << endl;
-
+    while (getline(file, line)) {
+        istringstream lineStream(line);
+        int num;
+        while (lineStream >> num) {
+            numbers.push_back(num);
+        }
+        int ris = findNum(numbers);
+        outputFile << ris << endl;
+        numbers.clear();
+    }
+    
     return 0;
 }
